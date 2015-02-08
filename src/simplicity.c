cd35e2e367922faf2251224707ad835b4f772d41
Eric Miller <eric@legoaces.org>
2015-02-08 07:37:49 -0500

    Spaces to tabs

--- src/simplicity.c
+++ src/simplicity.c
@@ -7,8 +7,8 @@ TextLayer *text_time_layer;
 Layer *line_layer;
 
 void line_layer_update_callback(Layer *layer, GContext* ctx) {
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
+	graphics_context_set_fill_color(ctx, GColorWhite);
+	graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
 }
 
 typedef struct{
@@ -40,25 +40,25 @@ Event nextEvent(){
 }
 
 void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
-  // Need to be static because they're used by the system later.
-  static char time_text[] = "00:00";
-  //static char date_text[] = "Xxxxxxxxx 00";
+	// Need to be static because they're used by the system later.
+	static char time_text[] = "00:00";
+	//static char date_text[] = "Xxxxxxxxx 00";
 
-  char *time_format;
+	char *time_format;
 
-  if (!tick_time) {
-    time_t now = time(NULL);
-    tick_time = localtime(&now);
-  }
+	if (!tick_time) {
+		time_t now = time(NULL);
+		tick_time = localtime(&now);
+	}
 
-  // TODO: Only update the date when it's changed.
-  //strftime(date_text, sizeof(date_text), "%B %e", tick_time);
-  //text_layer_set_text(text_date_layer, date_text);
+	// TODO: Only update the date when it's changed.
+	//strftime(date_text, sizeof(date_text), "%B %e", tick_time);
+	//text_layer_set_text(text_date_layer, date_text);
 	Event next = nextEvent();
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "{%s, %d}", next.name, next.time);
 	text_layer_set_text(text_date_layer, next.name);
 	
-	char deltaText[] = "in XXXX minutes  ";
+	char deltaText[] = "in XXXX minutes	 ";
 	
 	int dt = next.time - time(NULL);
 	snprintf(deltaText, sizeof(deltaText), "in %d minutes", (dt+30)/60);
@@ -66,65 +66,65 @@ void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
 	text_layer_set_text(text_delta_layer, deltaText);
 
 
-  if (clock_is_24h_style()) {
-    time_format = "%R";
-  } else {
-    time_format = "%I:%M";
-  }
+	if (clock_is_24h_style()) {
+		time_format = "%R";
+	} else {
+		time_format = "%I:%M";
+	}
 
-  strftime(time_text, sizeof(time_text), time_format, tick_time);
+	strftime(time_text, sizeof(time_text), time_format, tick_time);
 
-  // Kludge to handle lack of non-padded hour format string
-  // for twelve hour clock.
-  if (!clock_is_24h_style() && (time_text[0] == '0')) {
-    memmove(time_text, &time_text[1], sizeof(time_text) - 1);
-  }
+	// Kludge to handle lack of non-padded hour format string
+	// for twelve hour clock.
+	if (!clock_is_24h_style() && (time_text[0] == '0')) {
+		memmove(time_text, &time_text[1], sizeof(time_text) - 1);
+	}
 
-  text_layer_set_text(text_time_layer, time_text);
+	text_layer_set_text(text_time_layer, time_text);
 }
 
 void handle_deinit(void) {
-  tick_timer_service_unsubscribe();
+	tick_timer_service_unsubscribe();
 }
 
 void handle_init(void) {
-  window = window_create();
-  window_stack_push(window, true /* Animated */);
-  window_set_background_color(window, GColorBlack);
+	window = window_create();
+	window_stack_push(window, true /* Animated */);
+	window_set_background_color(window, GColorBlack);
 
-  Layer *window_layer = window_get_root_layer(window);
+	Layer *window_layer = window_get_root_layer(window);
 
-  text_date_layer = text_layer_create(GRect(8, 65, 144-8, 130-65));
-  text_layer_set_text_color(text_date_layer, GColorWhite);
-  text_layer_set_background_color(text_date_layer, GColorClear);
-  text_layer_set_font(text_date_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
-  layer_add_child(window_layer, text_layer_get_layer(text_date_layer));
+	text_date_layer = text_layer_create(GRect(8, 65, 144-8, 130-65));
+	text_layer_set_text_color(text_date_layer, GColorWhite);
+	text_layer_set_background_color(text_date_layer, GColorClear);
+	text_layer_set_font(text_date_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+	layer_add_child(window_layer, text_layer_get_layer(text_date_layer));
 
-  text_delta_layer = text_layer_create(GRect(8, 130, 144-8, 168-130));
-  text_layer_set_text_color(text_delta_layer, GColorWhite);
-  text_layer_set_background_color(text_delta_layer, GColorClear);
-  text_layer_set_font(text_delta_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
-  layer_add_child(window_layer, text_layer_get_layer(text_delta_layer));
+	text_delta_layer = text_layer_create(GRect(8, 130, 144-8, 168-130));
+	text_layer_set_text_color(text_delta_layer, GColorWhite);
+	text_layer_set_background_color(text_delta_layer, GColorClear);
+	text_layer_set_font(text_delta_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+	layer_add_child(window_layer, text_layer_get_layer(text_delta_layer));
 
-  text_time_layer = text_layer_create(GRect(7, 2, 144-7, 60-2));
-  text_layer_set_text_color(text_time_layer, GColorWhite);
-  text_layer_set_background_color(text_time_layer, GColorClear);
-  text_layer_set_font(text_time_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
-  layer_add_child(window_layer, text_layer_get_layer(text_time_layer));
+	text_time_layer = text_layer_create(GRect(7, 2, 144-7, 60-2));
+	text_layer_set_text_color(text_time_layer, GColorWhite);
+	text_layer_set_background_color(text_time_layer, GColorClear);
+	text_layer_set_font(text_time_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+	layer_add_child(window_layer, text_layer_get_layer(text_time_layer));
 
-  GRect line_frame = GRect(8, 60, 139, 2);
-  line_layer = layer_create(line_frame);
-  layer_set_update_proc(line_layer, line_layer_update_callback);
-  layer_add_child(window_layer, line_layer);
+	GRect line_frame = GRect(8, 60, 139, 2);
+	line_layer = layer_create(line_frame);
+	layer_set_update_proc(line_layer, line_layer_update_callback);
+	layer_add_child(window_layer, line_layer);
 
-  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
-  handle_minute_tick(NULL, MINUTE_UNIT);
+	tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+	handle_minute_tick(NULL, MINUTE_UNIT);
 }
 
 
 int main(void) {
-  handle_init();
-  app_event_loop();
-  
-  handle_deinit();
+	handle_init();
+	app_event_loop();
+	
+	handle_deinit();
 }
